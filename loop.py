9cee2ba11ad03ea1bfbdd985e179647117d93f75
Indhumathi K <44321007+Indhumathi-ML@users.noreply.github.com>
2020-09-19 16:53:46 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,7 @@
+import os
+
+i = 0
+while i < 10:
+    # os.system(pause)
+    os.system("/media/rathanlycan/8E9C149A9C147EC1/Indhu/ANPR/OpenCV_3_License_Plate_Recognition_Python-master/Main.py")
+    i += 1
